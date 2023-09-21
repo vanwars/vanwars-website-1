712e1aef0bb1e609f9dff3ba321bc9095bad3e1a
Sarah <vanwars@gmail.com>
2023-09-21 08:03:19 -0400

    WIP: Language updates.

--- assets/js/display-courses.js
+++ assets/js/display-courses.js
@@ -3,22 +3,18 @@ const toLinks = (courses) => {
         .map((course) => {
             let html = "";
             if (course.url) {
-                html += `
-                <a href="${course.url}" target="_blank">${course.period}</a>
-            `;
+                html += `<a href="${course.url}" target="_blank">${course.period}</a>`;
             } else {
                 html += `<span>${course.period}</span>`;
             }
-            if (course.notes) {
-                html += course.notes;
-            }
+            html += course.notes ? " " + course.notes : "";
             return html;
         })
         .join(" &bull; ");
 };
 
-const toRows = (sessions) => {
-    let html = "";
+const toTable = (sessions) => {
+    let html = "<table>";
     for (const key in sessions) {
         const currentSession = sessions[key];
         html += `<tr>
@@ -28,37 +24,54 @@ const toRows = (sessions) => {
             </td>
         </tr>`;
     }
+    html += "</table>";
+    return html;
+};
+
+const toList = (sessions) => {
+    let html = "<ul>";
+    for (const key in sessions) {
+        const currentSession = sessions[key];
+        html += `<li>
+            ${key}: </strong>
+                ${toLinks(currentSession)}
+        </li>`;
+    }
+    html += "</ul>";
     return html;
 };
 
 const renderCourse = (data) => {
     return `
-            <section class="teaching">
-                <section class="details">
-                    <h3>
-                        ${data.code}: ${data.title}
-                    </h3>
-                    <div class="course-links">
-                        <p>${data.description}</p>
-                        <table>
-                            ${toRows(data.sessions)}
-                        </table>
-                    </div>
-                </section>
-                <p class="years">${data.years}</p>
-            </teaching>
-            `;
+        <section class="teaching">
+            <section class="details">
+                <h3>
+                    ${data.code}: ${data.title}
+                </h3>
+                <div class="courses">
+                    <p>${data.description}</p>
+                    ${toTable(data.sessions)}
+                </div>
+            </section>
+            <p class="years">${data.years}</p>
+        </teaching>
+        `;
 };
+
 const displayCoursesTaught = async () => {
     const response = await fetch("/assets/data/courses.json");
     const data = await response.json();
     const el = document.querySelector(".teaching-section");
     for (const key in data) {
+        // header:
         el.insertAdjacentHTML("beforeend", `<h2>${key}</h2>`);
         const courses = data[key];
+
+        // associated courses:
         courses.forEach((course) => {
             el.insertAdjacentHTML("beforeend", renderCourse(course));
         });
     }
 };
+
 displayCoursesTaught();
